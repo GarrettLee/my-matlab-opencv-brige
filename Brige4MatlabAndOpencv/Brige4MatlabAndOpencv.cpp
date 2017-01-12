6f7c9203fa1b29d8dde47f090db4e61d5b199fa1
GarrettLee <672757671@qq.com>
2017-01-12 11:53:26 +0800

    添加项目文件。

---
+++ Brige4MatlabAndOpencv/Brige4MatlabAndOpencv.cpp
@@ -0,0 +1,22 @@
+// Brige4MatlabAndOpencv.cpp : 定义 DLL 应用程序的导出函数。
+//
+
+#include "stdafx.h"
+#include "Brige4MatlabAndOpencv.h"
+
+
+// 这是导出变量的一个示例
+BRIGE4MATLABANDOPENCV_API int nBrige4MatlabAndOpencv=0;
+
+// 这是导出函数的一个示例。
+BRIGE4MATLABANDOPENCV_API int fnBrige4MatlabAndOpencv(void)
+{
+    return 42;
+}
+
+// 这是已导出类的构造函数。
+// 有关类定义的信息，请参阅 Brige4MatlabAndOpencv.h
+CBrige4MatlabAndOpencv::CBrige4MatlabAndOpencv()
+{
+    return;
+}
